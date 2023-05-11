ba7d43b45ceeb1293f6ea6a962b14cf9116f1b7c
nikumyash <98515855+nikumyash@users.noreply.github.com>
2023-05-11 16:54:47 +0530

    first commit

--- routes/loginRoute.js
+++ routes/loginRoute.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const User = require('../model/User');
+const User = require('../models/User');
 const bcrypt = require('bcrypt')
 const {registerValidation,loginValidation} = require('../validation');
 const jwt = require('jsonwebtoken')
